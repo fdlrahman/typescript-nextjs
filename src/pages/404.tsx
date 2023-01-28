323e48c8d5884d55a806e3c268b10e99e87bf518
fdlrahman <jedacoding@gmail.com>
2023-01-28 08:44:18 +0800

    add custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Custom404() {
+  return (
+    <div>
+        <h1 className='title-not-found'>Ooooppsss...</h1>
+        <h3 className='title-not-found'>Halaman yang anda cari tidak ditemukan!</h3>
+    </div>
+  )
+}
